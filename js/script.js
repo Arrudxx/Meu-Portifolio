60428c0903c4ec27a80a43e61f384bbd5c6cdbfc
Arrudxx <d4niel.arruda@gmail.com>
2021-03-04 16:36:49 -0300

    arrumei algumas partes de resposividade em algumas paginas

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btnMenuMobile").addEventListener('click', function(){
+    document.getElementById("menuMobile").classList.toggle("disable");
+})
+
+document.getElementById("btnFechaMenuMobile").addEventListener('click', function(){
+    document.getElementById("menuMobile").classList.toggle("disable");
+})
